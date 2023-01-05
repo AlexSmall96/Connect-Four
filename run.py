d5232ed24e767585e77612085d8f552042119242
AlexSmall96 <alex.small739@gmail.com>
2023-01-05 18:52:37 +0000

    "Create function to display game board"

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+import time
+
+def print_board(board_data):
+    """
+    Prints the game board in its current state to the terminal
+    """
+    os.system('cls||clear')
+    print('-------------------------')
+    for row in board_data:
+        board_row=''
+        for entry in row:
+            if entry == 'red':
+                board_row +=' '+ '\033[2;31m' + 'o' + '\033[0;0m' ' '
+            elif entry == 'blue':
+                board_row +=' '+ '\033[2;34m' + 'o' + '\033[0;0m' ' '
+            else:
+                board_row +=' '+ entry + ' '
+        board_row='| ' + board_row + ' |'
+        print(board_row)
+    print('-------------------------')  
+    
+
+board_data=[['.' for i in range(7)] for j in range(6)]
+print_board(board_data)
+
+
