c8d7a9c7b909c8d48f3ab92b477560ae5646bc16
AlexSmall96 <alex.small739@gmail.com>
2023-01-06 17:50:22 +0000

    "Fix bug to allow game to stop when win is detected"

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from board import Board
+import os
+import sys
 
 def run_game():
+    sys.exit()
     play_again=True
     while play_again:
         data=[['.' for i in range(7)] for j in range(6)]
@@ -10,11 +13,11 @@ def run_game():
         color_cycle={'red':'blue','blue':'red'}
         count=0
         while board.running and count < 42:
+            color=color_cycle[color]
             column,counter_added=board.update_data(color)
             if counter_added:
-                color=color_cycle[color]
                 board.display()
-                game_running=board.check_winner(column,color)
+                board.running=board.check_winner(column,color)
                 count += 1
         print(f"Well Done {color}, you won!" )   
         play_again=input('Would you like to play again? (y/n) ')=='y'
