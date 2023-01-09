e4bb899b7dc644fe51943221a95b1cd52c39cc3b
AlexSmall96 <alex.small739@gmail.com>
2023-01-09 21:13:57 +0000

    "Move run_game function to setup.py"

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
-from board import Board
-import os
 import setup
-import time
+
 
 setup.print_title()
 setup.print_welcome()
@@ -10,31 +8,10 @@ setup.print_instructions()
 input('Press Enter to set up the game!')
 
 no_players = setup.set_no_players()
-player_1,player_2 = setup.set_usernames(no_players)
+usernames = setup.set_usernames(no_players)
+user_colors = setup.select_colors(usernames)
+setup.run_game(no_players,user_colors,usernames)
 
-play_again=True
-while play_again:
-    data=[['.' for i in range(7)] for j in range(6)]
-    board=Board(data)
-    board.display()
-    color='red'
-    color_cycle={'red':'yellow','yellow':'red'}
-    count=0
-    while board.running and count < 42:
-        column,counter_added=board.update_data_human(color)
-        if counter_added:
-            board.display()
-            board.running=board.check_winner(column,color)
-            count += 1
-            color=color_cycle[color]
-    color=color_cycle[color]
-    print(f"Well Done {color}, you won!" )   
-    play_again=input('Would you like to play again? (y/n) ')=='y'
-print("""
-Thank you for playing connect 4. 
-This programme was created by Alex Small. 
-Please visit my GitHub profile https://github.com/AlexSmall96.
-""")
     
 
 
--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+from board import Board
+import os
+import time
+
+
+
 def print_title():
     print('               ▄▀▀▀▀'+'\033[1;31m'+'    ▄▀▀▀▄'+'\033[0;0m'+'    ██   █    ██   █   █▀▀▀▀▀    ▄▀▀▀▀   ▀▀▀█▀▀▀')                 
     print('              █'+'\033[1;31m'+'        █     █'+'\033[0;0m'+'   █ █  █    █ █  █   █▄▄▄▄▄   █           █  ')                 
@@ -37,12 +43,12 @@ def set_no_players():
     correct_input = False
     while not correct_input:
         no_players = input("""
-        Select Number of Players:
-        Enter 1 to play human vs computer or 2 to play human vs human 
+Select Number of Players:
+Enter 1 to play human vs computer or 2 to play human vs human 
         """) 
         if no_players.isnumeric():
-            if int(no_players) == 1 or int(no_players) ==2:
-                return no_players
+            if int(no_players) == 1 or int(no_players) == 2:
+                return int(no_players)
                 correct_input=True
         else:
             print('Please enter either 1 or 2')
@@ -52,8 +58,55 @@ def set_no_players():
 def set_usernames(no_players):
     if no_players == 1:
         player_1 = input('Player 1 - Please enter your username ')
-        return player_1
+        return [player_1]
     else:
         player_1 = input('Player 1 - Please enter your username ')
         player_2 = input('Player 2 - Please enter your username ')
-        return player_1, player_2
+        return [player_1, player_2]
+
+def select_colors(usernames):
+    if len(usernames) == 1:
+        usernames.append('computer')
+    user_colors = {}
+    correct_input = False
+    while not correct_input: 
+        choice = input(f"{usernames[0]} Select a color : Red or Yellow ").lower()
+        if choice == 'red':
+            user_colors[usernames[0]] = 'red'
+            user_colors[usernames[1]] = 'yellow'
+            correct_input = True
+        elif choice == 'yellow':
+            user_colors[usernames[1]] = 'red'
+            user_colors[usernames[0]] = 'yellow'
+            correct_input = True
+    return user_colors
+            
+        
+            
+   
+
+def run_game(no_players,usercolors,usernames):
+    if no_players == 2:
+        play_again=True
+        while play_again:
+            data=[['.' for i in range(7)] for j in range(6)]
+            board=Board(data)
+            board.display()
+            color='red'
+            color_cycle={'red':'yellow','yellow':'red'}
+            count=0
+            while board.running and count < 42:
+                column,counter_added=board.update_data_human(color)
+                if counter_added:
+                    board.display()
+                    board.running=board.check_winner(column,color)
+                    count += 1
+                    color=color_cycle[color]
+            color=color_cycle[color]
+            print(f"Well Done {color}, you won!" )   
+            play_again=input('Would you like to play again? (y/n) ')=='y'
+        print("""
+Thank you for playing connect 4. 
+This programme was created by Alex Small. 
+Please visit my GitHub profile https://github.com/AlexSmall96.
+""")
